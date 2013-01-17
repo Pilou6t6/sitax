e628cbfc77ff8ec607a36f396e7111e0d9fee10d
polosson <polosson@EtoileNoire>
2013-01-17 19:25:15 +0100

    page "donnateurs"

--- pages/p_4donnateurs.php
+++ pages/p_4donnateurs.php
@@ -6,5 +6,13 @@
 
 
 <div class="pageContent">
-
+	<div class="ui-state-default pad5 ui-corner-all big gras">Voici les généreux donnateurs, mécènes du projet "Robert" :</div>
+	<div class="pad5 marge30l margeTop10">
+		<li class="gros">
+			<a href="http://www.acousmie.fr" target="_blank"><img src="http://www.acousmie.fr/gfx/logo-acousmie.png" width="150" /> ACOUSMIE</a> (initiateurs du projet) : <b>2000€</b>, <i>le 01/05/2012</i>
+		</li>
+		<li class="gros">
+			...
+		</li>
+	</div>
 </div>
