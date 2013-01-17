8ee5011b8d813537f98d1e63d9375b2152f0fab7
moutew <mathieu@d2mphotos.fr>
2013-01-17 15:24:03 +0100

    Page live_demo

--- pages/p_5live_demo.php
+++ pages/p_5live_demo.php
@@ -7,4 +7,13 @@
 
 <div class="pageContent">
 
+	<div style="position:absolute; bottom:0%; right:0%;text-align:center; font-size:4em; color:purple;">
+		Essayez Robert ! !<br />
+		<a href="http://www.robertdemo.polosson.com" target="_new"><img src="gfx/icones/berto.png"></a>
+		Login : demo@robertmanager.org <br />Pass : demo <br />
+
+		<span style="font-size:0.25em; color:black;">Illustration : Robert Bidochon par Binet</span>
+		</div>
+
+
 </div>
