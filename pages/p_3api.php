67c1f3afebce54cd5b97b0da3229035e1f873610
moutew <mathieu@d2mphotos.fr>
2013-01-17 17:04:51 +0100

    Systeme de recuperation des WIKI pour la doc sur la page API

--- pages/p_3api.php
+++ pages/p_3api.php
@@ -1,10 +1,26 @@
-<script>
-	$(function(){
-
-	});
-</script>
-
+<script src="js/markdown.js"></script>
 
 <div class="pageContent">
+
+	<div id="markdown_code" class="hide">
+		<textarea>
+			<?php
+				$file = '../robert.wiki/5)-Documentation-du-Robert-:-DÉVELOPPEMENT.md';
+				$handle = fopen($file, "r");
+				$contents = fread($handle, filesize($file));
+				fclose($handle);
+				echo $contents ;
+			?>
+		</textarea>
+	</div>
+
+	<div id="html_code"></div>
+
+<span><a href="https://github.com/evilstreak/markdown-js" target="_blank">powered by markdown-js</a> :)</span>
+
+<script>
+	txt = markdown.toHTML( $("#markdown_code").text() )
+	$("#html_code").html ( txt ) ;
+</script>
 
 </div>
