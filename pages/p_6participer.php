e702e1797d4bf3735ed4336a2e33014530688637
polosson <polosson@EtoileNoire>
2013-01-25 14:48:13 +0100

    Page participer : update de la méthode expliquée pour ajout de bug

--- pages/p_6participer.php
+++ pages/p_6participer.php
@@ -20,9 +20,6 @@
 	<p>Par où commencer ? C'est facile :</p>
 	<br />
 
-
-
-
 	<h2>Création d'un compte sur GitHub</h2>
 
 	<span class="floatLeft marge30r"><img src="gfx/git-tuto/create_account.jpg" width="500" border=2 /></span>
@@ -44,35 +41,45 @@
 
 	<h2>Ajouter un bug</h2>
 
-	<p class="floatRight marge30l">
-		Fenêtre d'ajout de bug du GitHub :<br /><br />
+	<p class="floatRight marge30l center">
+		Fenêtre d'ajout de bug sur GitHub<br /><br />
 		<img src="gfx/git-tuto/add_bug.jpg" width="600" border=2 />
 	</p>
 
-	<p><a href="https://github.com/RobertManager/robert/issues/new" target="_new" />Cliquez ici pour ajouter un bug maintenant !</a></p>
-
-	<u class="gras">Informations essentielles</u> :
-	<ul>
-		<li><b>Le titre</b> <b class="red">(1)</b> de votre ticket</li>
-		<li><b>Sa description</b> <b class="red">(2)</b> : Soyez le plus précis possible. Dans le cas d'un bug, essayez de le reproduire plusieurs fois
-			et décrivez la procédure pour que nous puissions le corriger</li>
-	</ul>
-	<u class="gras">Les labels</u> <b class="red">(3)</b> :<br />
-	Ils permettent de catégoriser les informations :
-	<ul>
-		<li><b>Bug</b> - quand quelque chose ne se passe pas comme prévu</li>
-		<li><b>wantMore</b> - Si vous avez une idée de fonction ou d'amélioration</li>
-		<li><b>Question</b> - Quelque chose que vous ne comprenez pas</li>
-	</ul>
-
-	<u class="gras">Mise en forme du texte</u> :<br />
-	Les raccourcis permettant de mettre votre texte en forme sont listés dans la fenêtre qui s'ouvre en cliquant sur le lien <b>"GitHub Flavored Markdown"</b> <b class="red">(4)</b>.
-	Il s'agit d'un mode rapide de mise en forme du texte.<br />
-	Par exemple, pour mettre en texte en gras, il faut mettre deux étoiles <small><i>(touche "multiplier")</i></small> qui entourent votre texte :
+	<hr />
+	<div class="gros">Il y a <b>DEUX manières</b> de procéder :
+		<ul>
+			<li>Via l'interface de la page <a href="index.php?go=7bugHunter">"bugHunter" du site</a>, grâce au bouton "+" en haut de colonne,</li>
+			<li>Via gitHub, en vous rendant sur <a href="https://github.com/RobertManager/robert/issues/new">le repo gitHub du Robert</a>, en vous connectant et en remplissant le formulaire "new Issue"</li>
+		</ul>
+		<small>Note : dans tous les cas, il vous faudra vous connecter à votre compte gitHub.</small>
+	</div>
+	<hr />
+	<p>
+		<u class="gras">Informations essentielles</u> :
+		<ul>
+			<li><b>Le titre</b> <b class="red">(1)</b> de votre ticket</li>
+			<li><b>Sa description</b> <b class="red">(2)</b> : Soyez le plus précis possible. Dans le cas d'un bug, essayez de le reproduire plusieurs fois
+				et décrivez la procédure pour que nous puissions le corriger</li>
+		</ul>
+		<u class="gras">Les labels</u> <b class="red">(3)</b> :<br />
+		Ils permettent de catégoriser les informations :<br />
+		<small>(Merci de n'en mettre qu'un seul à la fois, pour plus de clarté)</small>
+		<ul>
+			<li><b>Bug</b> - quand quelque chose ne se passe pas comme prévu</li>
+			<li><b>wantMore</b> - Si vous avez une idée de fonction ou d'amélioration</li>
+			<li><b>Question</b> - Quelque chose que vous ne comprenez pas</li>
+		</ul>
+	</p>
+	<p>
+		<u class="gras">Mise en forme du texte</u> :<br />
+		Les raccourcis permettant de mettre votre texte en forme sont listés dans la fenêtre qui s'ouvre en cliquant sur le lien <b>"GitHub Flavored Markdown"</b> <b class="red">(4)</b>.
+		Il s'agit d'un mode rapide de mise en forme du texte.<br />
+		Par exemple, pour mettre en texte en gras, il faut mettre deux étoiles <small><i>(touche "multiplier")</i></small> qui entourent votre texte :
+	</p>
 	<p align="center">**<b>Le texte a l'intérieur des étoiles sera en gras</b>**</p>
-	Quand vous avez terminé, vous pouvez cliquer sur l'onglet preview pour voir le formatage.
-
-	<p>Voilà, vous pouvez maintenant cliquer sur le bouton <b>SUBMIT NEW ISSUE</b> <b class="red">(5)</b>, pour que tous les développeurs reçoivent un mail qui les alerte de votre problème ou de votre suggestion</p>
+	<p>Quand vous avez terminé, vous pouvez cliquer sur l'onglet preview pour voir le formatage.</p>
+	<p>Voilà, vous pouvez maintenant cliquer sur le bouton <b>SUBMIT NEW ISSUE</b> <b class="red">(5)</b>.</p>
 
 	<h2>Merci d'avance pour votre participation !</h2>
 
